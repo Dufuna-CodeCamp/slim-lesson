c894c2fb7e344d16cdcbc33a4a6b83dc3b0c6f7f
Kehinde Ladipo <ladipokenny@gmail.com>
2020-12-24 18:19:33 +0100

    move web to bootstrap

--- bootstrap/database.php
+++ bootstrap/database.php
@@ -1,8 +1,11 @@
 <?php
 
+use Illuminate\Database\Capsule\Manager;
+
 $settings = $container->get('settings');
+
 $db_connection = $settings['db'];
-$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule = new Manager();
 $capsule->addConnection($db_connection);
 
 $capsule->bootEloquent();
